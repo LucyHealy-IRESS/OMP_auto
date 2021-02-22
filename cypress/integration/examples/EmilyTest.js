8d583aa0ae61cbab6e9d0f826f6a4b9c0a473a67
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-02-22 16:09:49 +0000

    Api call update

---
+++ cypress/integration/examples/EmilyTest.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Emilys Testing', () =>{
+
+    it('Create Client with Portfolios', () =>{
+        cy.Create_Client_API("cypress/fixtures/Clients/EmilyTest.xml");
+    })
+ 
+ })
