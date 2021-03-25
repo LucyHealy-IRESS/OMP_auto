9caf835d718a7677bf495a032b1b9ef00a0e7208
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-03-25 16:33:54 +0000

    added comments

--- cypress/integration/clientUI/create_UI Engine.js
+++ cypress/integration/clientUI/create_UI Engine.js
@@ -1,6 +1,7 @@
 import * as constants from "../../support/constants/Core.js";
 
-var filenameArray = Array(8).fill().map((_, i) => i); //creative array of consecutive numbers 0 - n
+var filenameArray = Array(8).fill().map((_, i) => i); //Due to Cypress limitations we can get the files in the folder and iterate that number of times. 
+//Instead we put a number here we know is greater than the number of xml files. Only tests will run that have an associated xml file, it wont crash!
 
 describe('Engine Iteration', () => {
 
@@ -15,7 +16,7 @@ describe('Engine Iteration', () => {
 
   filenameArray.forEach((filenameNo) => { //A for() doest work here
     it(`File No: ${filenameNo}`, () => {
-      if(filenameNo < filenames.length ){
+      if(filenameNo < filenames.length ){ //index check
         var fileNameToProcess = filenames[filenameNo];
         cy.log(fileNameToProcess);
         cy.ProcessFile(ExecutiionFolderLocation,fileNameToProcess);
