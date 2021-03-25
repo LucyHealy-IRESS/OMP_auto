57424459253dadfa0131262b6fde2994b904bd67
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-03-25 12:26:20 +0000

    Update create_UI Engine.js

--- cypress/integration/clientUI/create_UI Engine.js
+++ cypress/integration/clientUI/create_UI Engine.js
@@ -1,18 +1,17 @@
 import * as constants from "../../support/constants/Core.js";
 
-var filenameArray = Array(8).fill().map((_, i) => i);//creative array of consecutive numbers 0 - n
+var filenameArray = Array(8).fill().map((_, i) => i); //creative array of consecutive numbers 0 - n
 
 describe('Engine Iteration', () => {
-  filenameArray.forEach((filename) => {
+  filenameArray.forEach((filename) => { //A for() doest work here
     it(`File No: ${filename+1}`, () => {
-     cy.log(filename);
       var ExecutiionFolderLocation = "cypress/fixtures/ExecutionFolder/Create";
-      cy.task("ReadFolderDirectory", {path:ExecutiionFolderLocation,fileNo: filename}).then((fileNameToProcess) => {
+      cy.task("ReadFolderDirectory", {path:ExecutiionFolderLocation,fileNo: filename}).then((fileNameToProcess) => { //cant call a cy.task from outside a test (it)
         if(fileNameToProcess){
           cy.log(fileNameToProcess);
           cy.ProcessFile(ExecutiionFolderLocation,fileNameToProcess);
         }
-       });;
+       });
     });
   });
 });
