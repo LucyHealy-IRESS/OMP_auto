6518791ed71b205c9a3d73eca3f2d682f6479416
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-03-25 16:29:35 +0000

    one folder read refactor

--- cypress/integration/clientUI/create_UI Engine.js
+++ cypress/integration/clientUI/create_UI Engine.js
@@ -3,15 +3,23 @@ import * as constants from "../../support/constants/Core.js";
 var filenameArray = Array(8).fill().map((_, i) => i); //creative array of consecutive numbers 0 - n
 
 describe('Engine Iteration', () => {
-  filenameArray.forEach((filename) => { //A for() doest work here
-    it(`File No: ${filename+1}`, () => {
-      var ExecutiionFolderLocation = "cypress/fixtures/ExecutionFolder/Create";
-      cy.task("ReadFolderDirectory", {path:ExecutiionFolderLocation,fileNo: filename}).then((fileNameToProcess) => { //cant call a cy.task from outside a test (it)
-        if(fileNameToProcess){
-          cy.log(fileNameToProcess);
-          cy.ProcessFile(ExecutiionFolderLocation,fileNameToProcess);
-        }
-       });
+
+  var filenames;
+  var ExecutiionFolderLocation = "cypress/fixtures/ExecutionFolder/Create";
+
+  before(() => {   
+    cy.task("ReadFolderDirectory", {path:ExecutiionFolderLocation,fileNo: 1}).then((filenamesArray) => {
+      filenames = filenamesArray;
+    })
+  })
+
+  filenameArray.forEach((filenameNo) => { //A for() doest work here
+    it(`File No: ${filenameNo}`, () => {
+      if(filenameNo < filenames.length ){
+        var fileNameToProcess = filenames[filenameNo];
+        cy.log(fileNameToProcess);
+        cy.ProcessFile(ExecutiionFolderLocation,fileNameToProcess);
+      }      
     });
   });
 });
