3a7057f947784eb105c93f887b702eb889dfa6b2
Lucy Healy <lucy@omsystems.co.uk>
2021-03-26 13:05:17 +0000

    Update Portfolio_Assert.js

--- cypress/support/functions/Assert/Portfolio_Assert.js
+++ cypress/support/functions/Assert/Portfolio_Assert.js
@@ -3,11 +3,11 @@ import * as PortfolioConstants from "../../constants/portfolio.js";
 Cypress.Commands.add("Portfolio_Assert_1", (EntityData) => {
 
     cy.get('.btn-viewportfolios-action').click();
-    var entity = EntityData["PortfolioID"];
+    var entity = EntityData["Reference"];
     var ClientPortfolios = function(){
         cy.wait(1000)
         cy.get('#Client_ViewPortfolios > .gridContainer', {timeout:16000}).find('tr', {timeout:16000}).find('td').contains(entity).dblclick().then(function () {   
-
+            cy.wait(12000);
             cy.AssertEditor(PortfolioConstants.PortfoliosQuickEditorSelector,PortfolioConstants.PortfolioAccordioansSimple,EntityData);   
             debugger;
         });     
