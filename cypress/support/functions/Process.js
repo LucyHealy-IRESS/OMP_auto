fa1cb939c2e6f6d55936d341721c6269d2bd6d4f
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-03-29 11:26:39 +0100

    Update Process.js

--- cypress/support/functions/Process.js
+++ cypress/support/functions/Process.js
@@ -9,10 +9,10 @@ Cypress.Commands.add("ProcessFile", (ExecutiionFolderLocation,fileName) => {
       
       var fileLocation = ExecutiionFolderLocation + "/" + fileName;
       //1 Creation Pass
-      // cy.ProcessXMLFile(fileLocation, constants.RunType_Create, ActionFileNo);
+      cy.ProcessXMLFile(fileLocation, constants.RunType_Create, ActionFileNo);
 
       //2 Assert Pass
-      // cy.wait(3000);
+      cy.wait(3000);
        cy.ProcessXMLFile(fileLocation, constants.RunType_Assert,ActionFileNo);
       
        //3 Roll off what we have created
