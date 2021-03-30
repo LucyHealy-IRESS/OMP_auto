ed1e2156396753f038e9d61ed18618086d335755
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-03-30 10:43:50 +0100

    edit holdings units advanced

--- cypress/support/functions/Create/PortfolioInvestment_Create.js
+++ cypress/support/functions/Create/PortfolioInvestment_Create.js
@@ -6,7 +6,7 @@ Cypress.Commands.add("PortfolioInvestment_Create_1", (EntityData) => {
     cy.clickButtonInPopup(PortfolioConstants.PortfoliosQuickEditorSelector,"Pick funds from shortlist using fund name or ISIN");
     var buttonsSelector = "[aria-describedby='PortfolioSimpleEditor_QuickAddFundPopup']" + " .ui-dialog-buttonpane .ui-button span";
     var editRowButtonSelector = "[aria-describedby='PortfolioSimpleEditor_editRowPopup']" + " .ui-dialog-buttonpane .ui-button span";
-    cy.ProcessAddHoldingQuick(EntityData,buttonsSelector,'#PortfolioSimpleEditor_Holdings','#PortfolioAddPopUp','#PortfolioSimpleEditor_editRowPopup',editRowButtonSelector);
+    cy.ProcessAddHoldingQuick(EntityData,buttonsSelector,'#PortfolioSimpleEditor_Holdings','#PortfolioAddPopUp','#PortfolioSimpleEditor_editRowPopup',editRowButtonSelector,1);
 });
 
 Cypress.Commands.add("PortfolioInvestment_Create_2", (EntityData) => {
@@ -18,14 +18,15 @@ Cypress.Commands.add("PortfolioInvestment_Create_2", (EntityData) => {
     cy.clickButtonInPopup(PortfolioConstants.PortfoliosAdvancedEditorSelector,"Pick funds from shortlist using fund name or ISIN");
     var buttonsSelector = ".QuickAddFundPopup + .ui-dialog-buttonpane .ui-button span";
     var editRowButtonSelector = ".portfolio_Adv_CHoldings_editRowPopup + .ui-dialog-buttonpane .ui-button span";
-    cy.ProcessAddHoldingQuick(EntityData,buttonsSelector,'.PA_ClientHoldings_Accordian',PortfolioConstants.PortfoliosAdvancedEditorSelector,'.portfolio_Adv_CHoldings_editRowPopup',editRowButtonSelector);
+    cy.ProcessAddHoldingQuick(EntityData,buttonsSelector,'.PA_ClientHoldings_Accordian',PortfolioConstants.PortfoliosAdvancedEditorSelector,
+    '.portfolio_Adv_CHoldings_editRowPopup',editRowButtonSelector,2);
 });
 
 Cypress.Commands.add("PortfolioInvestment_Create_3", (EntityData) => {});
 
 
 //Will be moved but here for now due to merging conflicts 
-Cypress.Commands.add("ProcessAddHoldingQuick", (Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector) => {
+Cypress.Commands.add("ProcessAddHoldingQuick", (Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector,actionNo) => {
   cy.wait(2500); //allow control to prime
   var textInput = Entity.FundID;
    if (textInput) {
@@ -38,24 +39,49 @@ Cypress.Commands.add("ProcessAddHoldingQuick", (Entity, PopupSelector, HoldingsA
         cy.get(PopupSelector).contains(/^Add Selected Fund$/).click();
         cy.wait(1000);
         cy.get(PopupSelector).contains("Close").click();           
-      //   if(Entity.Units){
-      //     cy.clickAccordion_PreCheck(HoldingsAccordianSelector,editorSelector,"Holdings");
-      //     cy.wait(2000); //Allow for new fund to be added
-      //     cy.get('.CurrentHoldings_table tbody tr td').contains(SelectedFund).then(function($td){ //find the added fund in the holdings table
-      //       cy.wrap($td).closest("tr").find("button").find(".ui-icon-pencil").click().then(function(){ //click the associated edit icon
-      //         cy.get(editRowSelector).then(function(){ //check editor is open
-      //           cy.get(editRowSelector + " .RowEditUnits").type(`{selectall}`).type(Entity.Units);
-      //           cy.get(editRowButtonSelector).contains("OK").click();
-      //           cy.wait(1000);
-      //       })
-      //     })
-      //   });
-      // }
+        if(Entity.Units){
+          cy.clickAccordion_PreCheck(HoldingsAccordianSelector,editorSelector,"Holdings");
+          cy.wait(2000); //Allow for new fund to be added
+          cy.get('.CurrentHoldings_table tbody tr td').contains(SelectedFund).then(function($td){ //find the added fund in the holdings table
+            cy.wrap($td).closest("tr").find("button").find(".ui-icon-pencil").click().then(function(){ //click the associated edit icon
+              if(actionNo == 1){
+                cy.ProcessAddHoldingQuick_EditUnit_Simple(Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector);
+              }
+              else{
+                cy.ProcessAddHoldingQuick_EditUnit_Advanced(Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector);
+              }            
+          })
+        });
+      }
       })
     });
    }
 });
 
+
+Cypress.Commands.add("ProcessAddHoldingQuick_EditUnit_Simple", (Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector) => {
+  cy.get(editRowSelector).then(function(){ //check editor is open
+    cy.get(editRowSelector + " .RowEditUnits").type(`{selectall}`).type(Entity.Units);
+    cy.get(editRowButtonSelector).contains("OK").click();
+    cy.wait(1000);
+  })
+})
+
+Cypress.Commands.add("ProcessAddHoldingQuick_EditUnit_Advanced", (Entity, PopupSelector, HoldingsAccordianSelector, editorSelector, editRowSelector,editRowButtonSelector) => {
+  var UnitsCalculatorSelctor = ".portfolio_Adv_CHoldings_editHoldingUnitPopup";
+  var Element = Cypress.$(UnitsCalculatorSelctor);
+  
+  if(Element && Element.length > 0 ){
+    cy.get(UnitsCalculatorSelctor + " + .ui-dialog-buttonpane .ui-button span").contains("OK").click();
+    cy.wait(1000); //allow for save
+  }
+
+  cy.get(editRowSelector).then(function() { //check editor is open
+    cy.get(editRowSelector + " .RowEditUnits").type(`{selectall}`).type(Entity.Units);
+    cy.get(editRowButtonSelector).contains("OK").click();
+    cy.wait(1000);
+  })
+})
 
 
 Cypress.Commands.add("PortfolioInvestment_Tidy", (ActionFileNo) => {});
