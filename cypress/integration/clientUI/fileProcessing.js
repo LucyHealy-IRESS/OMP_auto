39d3fd904c6fad12ccb8060cfeddd0ed0effb8d8
Lucy Healy <lucy@omsystems.co.uk>
2021-03-31 12:16:38 +0100

    Update fileProcessing.js

--- cypress/integration/clientUI/fileProcessing.js
+++ cypress/integration/clientUI/fileProcessing.js
@@ -2,9 +2,8 @@ describe('Engine Iteration', () => {
   
 
     it('test', () => {
-        cy.login()
-        
-        var path = "cypress/screenshots/newFile5.csv";
+
+        var path = "cypress/screenshots/newFile6.csv";
         var content = "hello"
         cy.task("CreateFile", {filePath:path,content:content}).then(() => {
 
