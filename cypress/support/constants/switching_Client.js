9fee780af2e3c8ede2c5c382279b9961841533e1
Lucy Healy <lucy@omsystems.co.uk>
2021-04-29 16:40:00 +0100

    metadata based on the mapped selectors

--- cypress/support/constants/switching_Client.js
+++ cypress/support/constants/switching_Client.js
@@ -1,28 +1,202 @@
-import * as constants from "./constantsSelectors.js";
+import * as constants from"./constantsSelectors.js";
 export const
 
 ClientEditorSelector = '#SwitchingClientDetailsPopup',
 
 
 PersonalDetails = {
+  Title: {
+    Selector: "wijmo-wijinput-input ui-corner-all",
+    inputType: "String",
+    AccordianSelector:"",
+    AccordianName: "Personal  Details",
+  },
+  FirstName: {
+      Selector: "wijmo-wijinput-input ui-corner-all",
+      inputType: "String",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  Surname: {
+      Selector: "wijmo-wijinput-input ui-corner-all",
+      inputType: "String",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  DateOfBirth: {
+      Selector: "SwitchingDOB NonWijmoDatePickerElement day wijmo-wijinput-input ui-corner-all",
+      inputType: "Integer",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  DateOfBirth: {
+      Selector: "SwitchingDOB NonWijmoDatePickerElement month wijmo-wijinput-input ui-corner-all",
+      inputType: "Integer",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  DateOfBirth: {
+      Selector: "SwitchingDOB NonWijmoDatePickerElement year wijmo-wijinput-input ui-corner-all",
+      inputType: "Integer",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  Gender: {
+      Selector: "combobox wijmo-wijcombobox-input",
+      inputType: "Dropdown",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
+  TargetRiskProfile: {
+      Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+      inputType: "Dropdown",
+      AccordianSelector:"",
+      AccordianName: "Personal  Details",
+    },
 },
 
 FamilyDetails = {
+ MaritalStatus: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Family Details",
+  },
+Partner: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Family Details",
+  },
+NumberofDependentChildren: {
+    Selector: "ui-corner-all wijmo-wijinput-numeric wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Family Details",
+  },
+NumberofIndpendentChildren: {
+    Selector: "ui-corner-all wijmo-wijinput-numeric wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Family Details",
+  },
+NumberofSiblings: {
+    Selector: "ui-corner-all wijmo-wijinput-numeric wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Family Details",
+  },
 },
 
 ClientHealth = {
+  HealthIssues: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Health Details",
+  },
+  Smoker: {
+      Selector: "combobox wijmo-wijcombobox-input",
+      inputType: "Dropdown",
+      AccordianSelector:"",
+      AccordianName: "Health Details",
+    },
+  Postcode: {
+      Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+      inputType: "String",
+      AccordianSelector:"",
+      AccordianName: "Health Details",
+    },
 },
 
 Tax = {
+  TaxRegion: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Tax and Lifetime Allowance",
+  },
+  LifetimeAllowanceType: {
+      Selector: "combobox wijmo-wijcombobox-input",
+      inputType: "Dropdown",
+      AccordianSelector:"",
+      AccordianName: "Tax and Lifetime Allowance",
+  },
+  PreviouslyUsed: {
+      Selector: "",
+      inputType: "Integer",
+      AccordianSelector:"",
+      AccordianName: "Tax and Lifetime Allowance",
+  },
 },
 
 ServiceBasis = {
+ Autosetservicebasisbasedonassetsundermanagement: {
+    Selector: "wijmo-checkbox-box ui-widget ui-state-default ui-corner-all wijmo-checkbox-relative",
+    inputType: "Checkbox",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+ServiceBasis: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+EthicalInvestment: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+PayingFee: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+NextReviewVisitDD: {
+    Selector: "ClientNextReview NonWijmoDatePickerElement day ui-corner-all wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+NextReviewVisitMM: {
+    Selector: "ClientNextReview NonWijmoDatePickerElement month ui-corner-all wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+NextReviewVisitYYYY: {
+    Selector: "ClientNextReview NonWijmoDatePickerElement year ui-corner-all wijmo-wijinput-input",
+    inputType: "Integer",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+ClientStatus: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+Adviser: {
+    Selector: "combobox wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
+SharethisClient: {
+    Selector: "wijmo-checkbox-box ui-widget ui-state-default ui-corner-all wijmo-checkbox-relative",
+    inputType: "Checkbox",
+    AccordianSelector:"",
+    AccordianName: "Service Basis",
+  },
 },
 
 QuoteDetails = {
   Reference: {
-    Selector: ".Reference_tooltip input",
-    inputType: "String",
+    Selector:".Reference_tooltip input",
+    inputType:"String",
     AccordianSelector:"#QuoteDetails",
     AccordianName: 'Quote Details', 
   },
--- cypress/support/constants/switching_currentPlans.js
+++ cypress/support/constants/switching_currentPlans.js
@@ -16,7 +16,25 @@ BasicDetails = {
       inputType: "String",
       AccordianName: 'Basic Details',
       AccordianSelector: '.BasicDetailsAccordian',
-    }
+    },
+Provider: {
+      Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all ui-state-focus",
+      inputType: "Dropdown",
+      AccordianSelector: "",
+      AccordianName: "Basic Details",
+    },
+IncludeinConsolidatedReport: {
+      Selector: "wijmo-checkbox-box ui-widget ui-state-default ui-corner-all wijmo-checkbox-relative ui-state-active",
+      inputType: "Checkbox",
+      AccordianSelector: "",
+      AccordianName: "Basic Details",
+    },
+Isworkplacepension: {
+      Selector: "wijmo-checkbox-box ui-widget ui-state-default ui-corner-all wijmo-checkbox-relative ui-state-active",
+      inputType: "Checkbox",
+      AccordianSelector: "",
+      AccordianName: "Basic Details",
+    },
 },
 
 CurrentValue = { 
@@ -26,6 +44,30 @@ CurrentValue = {
     AccordianName: 'Current Value',
     AccordianSelector: '.CurrentValueAccordian',
   }, 
+AsAtDate: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all ui-input-trigger-right",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Current Value",
+  },
+AsAtDate: {
+    Selector: "",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Current Value",
+  },
+AsAtDate: {
+    Selector: "",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Current Value",
+  },
+TransferValue: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Current Value",
+  },
 },
 
 
@@ -41,11 +83,205 @@ SuppliedMaturityValues = {
     inputType: "Integer",
     AccordianName: 'Supplied Maturity Values',
     AccordianSelector: '.SuppliedMaturityValuesAccordian',
-  }
+  },
+ProjectedMaturityBasis: {
+    Selector: "projectedMaturityBasis wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+MaturityValuesProjected: {
+    Selector: "maturityValuesProjected wijmo-wijcombobox-input",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+Term: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+LowRateMaturityValue: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+MidRateMaturityValue: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+HighRateMaturityValue: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+AMC: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+AnnualProductChargesFundCharge: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+AnnualProductChargesPlanCharge: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+AnnualProductChargesPolicyFee: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
+AnnualProductchargesAdviserCharge: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Supplied Maturity Values",
+  },
 },
+
+GrowthRates = {
+NominalLow: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+  },
+NominalMid: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+ },
+NominalHigh: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+  },
+InflationAdjustedLow: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+  },
+InflationAdjustedMid: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+  },
+InflationAdjustedHigh: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Growth Rates",
+  },
+}
+
+OngoingContributions = {
+
+GrossPremium: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+Frequency: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+Escalation: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+OtherEscalation: {
+    Selector: "wijmo-wijinput ui-widget ui-helper-clearfix ui-state-default ui-corner-all",
+    inputType: "Integer",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+PolicyAnniversary: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+IncreasingEvery: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+ClientExpenses: {
+    Selector: "wijmo-wijcombobox-wrapper ui-state-default ui-corner-all",
+    inputType: "Dropdown",
+    AccordianSelector: "",
+    AccordianName: "Ongoing Contributions",
+  },
+}
+
+PaidUpMaturityValues = {
+PUPMaturityValueLow: {
+    Selector: "PUPMaturityValueLow_tooltip",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+PUPMaturityValueMid: {
+    Selector: "PUPMaturityValueMid_tooltip",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+PUPMaturityValueHigh: {
+    Selector: "PUPMaturityValueHigh_tooltip",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+AsAtDate: {
+    Selector: "PupMaturityValuesAsAtDate NonWijmoDatePickerElement day ui-corner-all wijmo-wijinput-input",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+AsAtDate: {
+    Selector: "PupMaturityValuesAsAtMonth NonWijmoDatePickerElement day ui-corner-all wijmo-wijinput-input",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+AsAtDate: {
+    Selector: "PupMaturityValuesAsAtYearNonWijmoDatePickerElement day ui-corner-all wijmo-wijinput-input",
+    inputType: "",
+    AccordianSelector: "",
+    AccordianName: "Paid Up Maturity Values",
+  },
+}
 
 AllCurrentPlanInputs = {
   ...BasicDetails,
   ...CurrentValue,
-  ...SuppliedMaturityValues
+  ...SuppliedMaturityValues,
+  ...GrowthRates,
+  ...OngoingContributions,
+  ...PaidUpMaturityValues
+
 }
