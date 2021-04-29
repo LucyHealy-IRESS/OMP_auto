5fbaffbbb565f2c84c9c824eddb7c4685f98d840
emilyhedgecock-iress <72869374+emilyhedgecock-iress@users.noreply.github.com>
2021-04-29 11:03:42 +0100

    switching adviser charges

---
+++ cypress/support/functions/Assert/Switching_AdviserCharges_Assert.js
@@ -0,0 +1,7 @@
+
+Cypress.Commands.add("Switching_AdviserCharges_Assert_1", (EntityData) => {});
+    
+Cypress.Commands.add("Switching_AdviserCharges_Assert_2", (EntityData) => {});
+    
+Cypress.Commands.add("Switching_AdviserCharges_Assert_3", (EntityData) => {});
+    
