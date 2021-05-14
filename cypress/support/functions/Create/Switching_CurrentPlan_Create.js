b005b943bc46533e294936ad25010cbc2c7262f9
emilyhedgecock-iress <emily.hedgecock@iress.com>
2021-05-14 14:49:46 +0100

    switching updates

--- cypress/support/functions/Create/Switching_CurrentPlan_Create.js
+++ cypress/support/functions/Create/Switching_CurrentPlan_Create.js
@@ -3,14 +3,16 @@ import * as constants from "../../constants/Core";
 
 
 Cypress.Commands.add("Switching_CurrentPlan_Create_1", (EntityData,CurrentPlanNo) => {
-    cy.log(CurrentPlanNo)
+    cy.log("Add cp no: " + CurrentPlanNo)
     if(CurrentPlanNo > 0){ //this isnt the first current plan (which opens the editor by auto, so need to click "Add Plan first")
+        cy.wait(7000); //let previous current plan save
         cy.clickButtonInPopup(swContants.CurrentPlanListSelector, 'Add Plan')
     }
 
     cy.get(swContants.CurrentPlanEditorSelector,{ timeout: constants.Timeout_GetQuoteResults  }).then(function(){    
          cy.PopulateEditor(swContants.CurrentPlanEditorSelector,swContants.AllCurrentPlanInputs,EntityData,"Switching Current Plan Editor");
          cy.wait(1000);      
+         cy.clickButtonInPopup(swContants.CurrentPlanEditorSelector,"Ok");    //Close the open current plan editor
      })
 });
     
