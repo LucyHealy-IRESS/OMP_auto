2f3c3f916c160b2a85b554d6252df876b4fb5b06
Vidhya Vasudevan <vidhya.vasudevan@iress.com>
2021-09-02 09:18:57 +0100

    Code added in Partner_Assert.js for opening the edit partner popup

--- cypress/support/functions/Assert/Partner_Assert.js
+++ cypress/support/functions/Assert/Partner_Assert.js
@@ -2,6 +2,7 @@ import * as ClientConstants from "../../constants/client.js";
 import * as Constants from "../../constants/Core.js";
 
 Cypress.Commands.add("Partner_Assert_1", (EntityData) => {
-
+    cy.wait(2000); //allow for client summary menu to auto open
+    cy.menuClick('Client Summary', ClientConstants.AddPartnerButtonSelector); //Click Client Summary menu and click edit partner button
 
 })
